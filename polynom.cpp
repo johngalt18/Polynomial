402cff81a6be0441bb9af4c84b3556dd5845fee4
Yuriy Samusevich <yuri@kromephotos.com>
2020-03-19 21:23:39 +0500

    [UPD] more samples of using class

--- polynom.cpp
+++ polynom.cpp
@@ -7,13 +7,23 @@ int main()
    setlocale(LC_ALL, "Russian"); // русский язык
    Polynomial p1(3, {14, 4, 8, 5}); 
    p1.output();
-   Polynomial p2(2, {5, 6, 12});
+   Polynomial p2(2, {2, 5, 3});
    p2.output();
    Polynomial p3(p1.get_degree() + p2.get_degree());
    p3 = p1 * p2;
+   cout << "Результат перемножения полиномов: " << endl;
    p3.output();
    Polynomial p4(p1.get_degree() * 3);
    p4 = p1 ^ 2; 
+   cout << "Результат возведения первого полинома в степень 2: " << endl;
    p4.output();
-   cout << "Дискриминант полинома: " << p4.discriminant() << endl;
+   Polynomial p5(10);
+   p5 = p4.derivative();
+   cout << "Производная текущего полинома: " << endl;
+   p5.output();
+   Polynomial p6(2);
+   p6 = p5.antiderivative();
+   cout << "Интеграл: " << endl;
+   p6.output();
+   // cout << "Дискриминант полинома 2: " << p2.discriminant() << endl;
 };
